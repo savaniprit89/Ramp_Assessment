9d7a5996bfff41f4e8592c95d72f6e22cd8527e5
pritsavani <savaniprit89@gmail.com>
2024-10-22 17:00:40 -0500

    first commit

---
+++ src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,42 @@
+import { useCallback } from "react"
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+  const setTransactionApproval = useCallback(
+    async (newValue: boolean) => {
+      await consumerSetTransactionApproval({
+        transactionId: transaction.id,
+        newValue,
+      })
+    },
+    [consumerSetTransactionApproval, transaction.id]
+  )
+
+  return (
+    <div className="RampPane">
+      <div className="RampPane--content">
+        <p className="RampText">{transaction.merchant}</p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="RampText--hushed RampText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={transaction.approved}
+        disabled={loading}
+        onChange={setTransactionApproval}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
